27624db6037210f34e53e33fd545e3fe18d2119d
mausch <mauricioscheffer@gmail.com>
2013-03-22 19:01:11 -0300

    Test wrappers

--- MbUnit.FSharp/Wrappers.fs
+++ MbUnit.FSharp/Wrappers.fs
@@ -1,3 +1,19 @@
 ﻿module MbUnit.FSharp
 
+open MbUnit.Framework
 
+let testList name tests = 
+    let suite = TestSuite name
+    Seq.iter suite.Children.Add tests
+    suite :> Test
+
+let testCase name (test: unit -> unit) = 
+    TestCase(name, Gallio.Common.Action test) :> Test
+
+let testFixture setup = 
+    Seq.map (fun (name, partialTest) ->
+                testCase name (setup partialTest))
+
+let testParam param =
+    Seq.map (fun (name, partialTest) ->
+                testCase name (partialTest param))
