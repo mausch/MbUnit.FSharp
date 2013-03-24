2c17f30a8ec87063b1275314d700bfb6c6f522d6
mausch <mauricioscheffer@gmail.com>
2013-03-23 21:10:19 -0300

    Assembly info

---
+++ MbUnit.FSharp/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace MbUnit.FSharpAssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyTitle("MbUnit.FSharp")>]
+[<assembly: AssemblyDescription("F# DSL for first-class MbUnit tests")>]
+[<assembly: AssemblyProduct("MbUnit.FSharp")>]
+[<assembly: AssemblyCopyright("Copyright Mauricio Scheffer 2013")>]
+do()
