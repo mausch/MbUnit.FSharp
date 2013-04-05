f0c5e5af2b19fce24deb9391f2833687182ae978
mausch <mauricioscheffer@gmail.com>
2013-04-04 23:59:11 -0300

    TestCaseBuilder improvements

--- MbUnit.FSharp/Wrappers.fs
+++ MbUnit.FSharp/Wrappers.fs
@@ -1,5 +1,6 @@
 ﻿module MbUnit.FSharp
 
+open System
 open MbUnit.Framework
 
 let testList name tests = 
@@ -14,7 +15,26 @@ let testFixture setup =
     Seq.map (fun (name, partialTest) ->
                 testCase name (setup partialTest))
 
-type TestCaseBuilder(name: string) = 
+type TestCaseBuilder(name) = 
+    member x.TryFinally(f, compensation) = 
+        try
+            f()
+        finally
+            compensation()
+    member x.TryWith(f, catchHandler) = 
+        try
+            f()
+        with e -> catchHandler e
+    member x.Using(disposable: #IDisposable, f) =
+        try
+            f disposable
+        finally
+            match disposable with
+            | null -> () 
+            | disp -> disp.Dispose()
+    member x.For(sequence, f) = 
+        for i in sequence do f i
+    member x.Combine(f1, f2) = f1
     member x.Zero() = ()
     member x.Delay f = f
     member x.Run f = testCase name f
