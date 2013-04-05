cfc1716caf591f5cc07276c68803117e0e2dd2fc
mausch <mauricioscheffer@gmail.com>
2013-04-05 00:34:23 -0300

    Version 1.1

--- MbUnit.FSharp/AssemblyInfo.fs
+++ MbUnit.FSharp/AssemblyInfo.fs
@@ -4,8 +4,8 @@ open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("1.1.0.0")>]
+[<assembly: AssemblyFileVersion("1.1.0.0")>]
 [<assembly: AssemblyTitle("MbUnit.FSharp")>]
 [<assembly: AssemblyDescription("F# DSL for first-class MbUnit tests")>]
 [<assembly: AssemblyProduct("MbUnit.FSharp")>]
